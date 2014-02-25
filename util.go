cd9d1b704944c525f06178883e4946684c5ac962
xuyu <xuyu16@gmail.com>
2014-02-25 23:08:30 +0800

    util: add file digest and read file perm

--- util.go
+++ util.go
@@ -1,7 +1,12 @@
 package spinner
 
 import (
+	"crypto/md5"
+	"encoding/hex"
+	"io/ioutil"
+	"os"
 	"strconv"
+	"strings"
 )
 
 func MustInt64(s string) int64 {
@@ -18,4 +23,30 @@ func MustInt(s string) int {
 		panic(err)
 	}
 	return i
+}
+
+func ReadVersion(filename string) string {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return ""
+	}
+	return strings.TrimSpace(string(data))
+}
+
+func FileMd5(filename string) (string, error) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return "", err
+	}
+	h := md5.New()
+	h.Write(data)
+	return hex.EncodeToString(h.Sum(nil)), nil
+}
+
+func FilePerm(filename string, perm os.FileMode) (os.FileMode, error) {
+	info, err := os.Stat(filename)
+	if err != nil {
+		return perm, err
+	}
+	return info.Mode(), nil
 }
