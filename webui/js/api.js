226acd1e034afb95519e3c410e59458e2d4ae7da
xuyu <xuyu16@gmail.com>
2014-02-27 18:22:32 +0800

    webui: update style and js

--- webui/js/api.js
+++ webui/js/api.js
@@ -12,23 +12,28 @@ function trigger_ul_head_click(ev){
 
 function group_tree_api(){
 	$.getJSON("/spinner/webui/tree", function(data){
-		$("#datacenter-name").text(data.Name);
-		$("#datacenter-location").text(data.Location);
-		var groups = $("#dt-groups");
+		g_datacenter_name.text(data.Name);
+		g_datacenter_location.text(data.Location);
 		$.each(data.Groups, function(index, gp) {
 			var html = "<ul class='col-xs-6'><h3 class='dt-group-head'>" + gp.Name;
 			html = html + "<span>" + gp.Machines.length + "</span></h3>";
 			$.each(gp.Machines, function(index, ma){
 				var cla = "";
-				if (ma.KeepAlive == 0 || ma.KeepAlive == "" || Math.abs(ma.KeepAlive - (new Date()).getTime()/1000) > 300) {
+				if (ma.KeepAlive == undefined) {
+					cla = "miss";
+				} else if (ma.KeepAlive == 0) {
+					cla == "miss";
+				} else if (Math.abs(ma.KeepAlive - (new Date()).getTime()/1000) > 300) {
 					cla = "miss";
 				} else {
 					cla = "alive";
 				}
-				html = html + "<li class='" + cla + "'><a href='#'>" + ma.IP + "@" + ma.Hostname + "</a></li>"
+				html = html + "<li class='" + cla;
+				html = html + "'><a href=\"javascript: task('dashboard', '" + ma.Hostname + "');\">";
+				html = html + ma.IP + " @ " + ma.Hostname + "</a></li>"
 			});
 			html = html + "</ul";
-			groups.html(html);
+			g_group_tree.html(html);
 			$(".dt-group-head").click(trigger_ul_head_click);
 		})
 	})
--- webui/js/application.js
+++ webui/js/application.js
@@ -1,4 +1,73 @@
+// menu-nav
+var g_menu_nav = $("#menu-nav");
+var g_menu_nav_open = false;
+
+function open_menu_nav(){
+	g_menu_nav.addClass("menu-nav-open");
+	g_menu_nav_open = true;
+}
+
+function close_menu_nav(){
+	g_menu_nav.removeClass("menu-nav-open");
+	g_menu_nav_open = false
+}
+
+function trigger_menu_nav(){
+	if (g_menu_nav_open) {
+		close_menu_nav()
+	} else {
+		open_menu_nav()
+	}
+}
+
+// task
+var g_cur_task = undefined;
+var g_cur_hostname = undefined;
+var g_cur_machine = $("#current-machine");
+
+function change_cur_task(name){
+	if (name != undefined && name != null && name != "") {
+		g_cur_task = name;
+	}
+}
+
+function change_cur_hostname(hostname){
+	if (hostname != undefined && hostname != null && hostname != "") {
+		g_cur_hostname = hostname;
+		g_cur_machine.text("@" + hostname);
+	}
+}
+
+function close_last_task(){
+	$(".cur-task").removeClass("cur-task");
+}
+
+function task(name, hostname){
+	if (name == g_cur_task) return
+	close_last_task()
+	change_cur_task(name);
+	change_cur_hostname(hostname);
+	switch (name) {
+		case "group_tree":
+			$("#dt-group-tree").addClass("cur-task");
+			group_tree_api();
+		break
+		case "trust_central":
+		break
+		case "dashboard":
+		break
+		case "file_system":
+		break
+		case "terminal":
+		break
+	}
+}
+
+// datacenter label
+var g_datacenter_name = $("#datacenter-name");
+var g_datacenter_location = $("#datacenter-location");
+var g_group_tree = $("#dt-group-tree");
+
 $(document).ready(function(){
 	open_menu_nav();
-	group_tree_api();
 })
