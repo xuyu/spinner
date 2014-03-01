37ce065e67989a147bb6a3c8097df87d7f573f16
xuyu <xuyu16@gmail.com>
2014-03-01 17:18:10 +0800

    node: set 1 minute timeout deadline for terminal command

--- node/terminal.go
+++ node/terminal.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"log"
 	"net/http"
 	"os/exec"
 	"strings"
+	"time"
 )
 
 // Terminal handles http request which executes shell commands like a linux terminal
@@ -15,12 +17,34 @@ func Terminal(rw http.ResponseWriter, req *http.Request) {
 		rw.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	output, err := exec.Command("/bin/sh", "-c", cmd).CombinedOutput()
-	if err != nil {
+	command := exec.Command("/bin/sh", "-c", cmd)
+	var buf bytes.Buffer
+	command.Stdout = &buf
+	command.Stderr = &buf
+	if err := command.Start(); err != nil {
 		internalServerError(rw, err)
-		log.Printf("terminal execute command [%s] error: %s", cmd, err.Error())
-	} else {
-		log.Printf("terminal execute command [%s]", cmd)
+		log.Println(err.Error())
+		return
 	}
-	rw.Write(output)
+	ch := make(chan error)
+	go func(ch chan<- error) {
+		ch <- command.Wait()
+	}(ch)
+	select {
+	case <-time.After(time.Minute):
+		if p := command.Process; p != nil {
+			p.Kill()
+		}
+		rw.WriteHeader(http.StatusRequestTimeout)
+		log.Printf("terminal command [%s] timeout", cmd)
+		return
+	case err := <-ch:
+		if err != nil {
+			internalServerError(rw, err)
+			log.Printf("terminal command [%s] error: %s", cmd, err.Error())
+			return
+		}
+		rw.Write(buf.Bytes())
+		log.Printf("terminal command [%s]", cmd)
+	}
 }
