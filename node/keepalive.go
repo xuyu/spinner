c208724be0a15b14eb4fc7457a0b35aef68d3aff
xuyu <xuyu16@gmail.com>
2014-03-02 23:45:36 +0800

    node: keepalive duration set by flag -keepalive

--- node/keepalive.go
+++ node/keepalive.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"net/url"
 	"time"
 )
+
+var (
+	keepAliveDuration int
+)
+
+func init() {
+	flag.IntVar(&keepAliveDuration, "keepalive", 180, "duration send keepalive with central server")
+}
 
 func keepAlive() {
 	u := fmt.Sprintf("%s/spinner/central/keepalive?hostname=%s", centralSpinner, url.QueryEscape(hostname))
@@ -17,6 +26,6 @@ func keepAlive() {
 		} else if resp.StatusCode != http.StatusOK {
 			log.Printf("%s %s", u, resp.Status)
 		}
-		time.Sleep(3 * time.Minute)
+		time.Sleep(keepAliveDuration * time.Second)
 	}
 }
